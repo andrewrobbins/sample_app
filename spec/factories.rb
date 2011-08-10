4bfa3c030f29f49755fe42eb3ea9fac289027a4c
Andrew Robbins <robbins_andrew@yahoo.com>
2011-08-10 15:55:09 -0700

    Forgot to add new files

---
+++ spec/factories.rb
@@ -0,0 +1,6 @@
+Factory.define :user do |user|
+  user.name                  "Michael Hartl"
+  user.email                 "mhartl@example.org"
+  user.password              "foobar#92"
+  user.password_confirmation "foobar#92"
+end
